950a7e6d12676f6c2bff5386eed34b1b4c6e1ebb
joforikwakye <jeffkwakye4u@gmail.com>
2023-03-21 10:08:06 +0000

    created the pop function

--- linked_list.py
+++ linked_list.py
@@ -35,3 +35,18 @@ class LinkedList:
             self.head.next = new_node
         self.length += 1
         return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
