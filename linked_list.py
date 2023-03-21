634424a979f6fb2c96eaa9ca6b3a2e286c8cbc7f
joforikwakye <jeffkwakye4u@gmail.com>
2023-03-21 09:53:07 +0000

    reated the prepend function

--- linked_list.py
+++ linked_list.py
@@ -18,3 +18,14 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
         return True
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.next = new_node
+        self.length += 1
+        return True
