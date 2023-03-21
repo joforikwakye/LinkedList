d0e44d0b4091b2a7649ae482337e13197d7a53de
joforikwakye <jeffkwakye4u@gmail.com>
2023-03-21 09:38:24 +0000

    created a linked list constructor

---
+++ linked_list.py
@@ -0,0 +1,10 @@
+from node import Node
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    
